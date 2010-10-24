19baeb237ff65390264751750f43ff417494bbfe
Chris Mitchell <chrism@lclark.edu>
2010-10-24 02:14:46 -0700

    More hacking away. I think I should just use the db_query method I wrote and hard code SQL for now. The active record is a bit beyond me right now; the approach might be more clear later.

--- model.py
+++ model.py
@@ -2,39 +2,63 @@ import sqlite3
 from gradebook import app
 from contextlib import closing
 
-def init_db(filename):
+def init_db():
     with closing(sqlite3.connect(app.config['DATABASE'])) as db:
         with app.open_resource("schema.sql") as f:
             db.cursor().executescript(f.read())
+        with app.open_resource("testdata.sql") as f:
+            db.cursor().executescript(f.read())
         db.commit()
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
 
 class Model(object):
     """A superclass for all active records."""
-    db = sqlite3.connect(app.config['DATABASE'])
-
+    db = connect_db()
+    _table = None #Name of the table
     def __init__(self):
         self._in_db = False #True, if the object was pulled from the db
+
+    @classmethod
+    def _query_db(cls, query, args=()):
+        """Return a list of dictionaries representing the results of
+        cursor.execute(query, args)"""
+        cursor = cls.db.execute(query, args)
+        attributes = [x[0] for x in cursor.description]
+        rows = [dict(zip(attributes, row)) for row in cursor.fetchall()]
+        return rows
+
+    @classmethod
+    def get(cls, pk=None):
+        if pk:
+            return cls._query_db("SELECT * FROM ? WHERE pk=?;", 
+                    (cls._table, pk, ))
+        else:
+            return cls._query_db("SELECT * FROM ?;", (cls._table, ))
+
+    def update(self):
+        pass
+
+    def delete(self, pk=None):
 
     def save(self):
         """INSERT or UPDATE the object into the db as necessary."""
         pass
-    def create(self):
-        pass
-    def get(self):
-        pass
+
 
 class Person(Model):
-    _table_name = 'person'
-    def __init__(self, first_name=None, last_name=None, alias=None,
-            grad_year=None, email=None):
+    def __init__(self, pk=None, first_name=None, last_name=None, alias=None,
+            grad_year=None, email=None, in_db=False):
         self.first_name = first_name
         self.last_name = last_name
         self.alias = alias
         self.grad_year = grad_year
         self.email = email
+        self._in_db = in_db
 
     def save(self):
-        db.execute
+        Model._query_db(
 
     def __str__(self):
         if self.first_name and self.last_name:
@@ -43,9 +67,13 @@ class Person(Model):
             return self.first_name
         if self.last_name:
             return self.last_name
+
     @classmethod
-    def get(pk=None):
-        pass
+    def get(cls, pk=None):
+        if pk:
+            return cls._query_db("SELECT * FROM person WHERE pk=?;", (pk, ))
+        else:
+            return cls._query_db("SELECT * FROM person;")
 
 
 class Assignment(Model):
@@ -55,4 +83,7 @@ class Grade(Model):
     pass
 
 if __name__ == "__main__":
+    #init_db()
+    print Person.get()
+
     p = Person(first_name="Chris")
